4b9d87278a02ba8ea6b393c60b87a473e5ea854b
Matt Gaddes <matt.gaddes@datagrail.io>
2022-03-25 07:06:45 -0700

    Complete 04 extra cred

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -10,17 +10,27 @@ function useToggle() {
 
   // 🐨 Add a property called `togglerProps`. It should be an object that has
   // `aria-pressed` and `onClick` properties.
-  const togglerProps = {'aria-pressed': on, onClick: toggle};
-  return {on, toggle, togglerProps}
+  const getTogglerProps = (customProps) => ({
+    'aria-pressed': on,
+    onClick: toggle,
+    ...customProps,
+  });
+  return {on, toggle, getTogglerProps}
 }
 
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({on})} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+      >
         {on ? 'on' : 'off'}
       </button>
     </div>
